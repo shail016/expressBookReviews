fc0b598cfb9ade7b82ba87737830df4e6657752a
Shailesh Aswal <aswalshail@yahoo.co.in>
2023-02-03 07:54:31 -0500

    initial changes step 1

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -9,20 +9,69 @@ const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => { //returns boolean
+    //write code to check if username and password match the one we have in records.
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    return (validusers.length > 0);
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+    //Write your code here
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username ) {
+        return res.status(404).json({ message: "Error logging in, username not provided!" });
+    }
+    if(!password){
+        return res.status(404).json({ message: "Error logging in, password not provided!" });
+    }
+
+    if (authenticatedUser(username, password)) {
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Please check username & password" });
+    }
 });
 
 // Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.put("/auth/review/:isbn", (req, res, next) => {
+    //Write your code here
+    const username = req.session?.authorization?.username;
+    const review = req.query.review;
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+    //   adds the review for user else updates the old review
+    // console.log("review: " + review + ", isbn: " + isbn + ", user: " + username);
+    if (book.review && book?.review[username]) {
+        book.review[username] = review;
+    } else {
+        book.review = { [username]: review }
+    }
+    // console.log(books[isbn].review);
+    return res.status(200).send("User Review successfully added");
+});
+
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const username = req.session?.authorization?.username;
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+    if (book.review && book?.review[username]) {
+        delete book.review[username];
+    }
+    console.log(book.review)
+    return res.status(200).send("User Review successfully deleted");
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,40 +4,79 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const exists = (username) => {
+    let userswithsamename = users.filter((user) => {
+       return user.username === username;
+    });
+    return (userswithsamename.length > 0);
+}
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.post("/register", (req, res) => {
+    //Write your code here
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username && password) {
+        if (exists(username)) {
+            return res.status(404).json({ message: "User already exists!" });
+        } else {
+            users.push({ "username": username, "password": password });
+            return res.status(200).json({ message: "User successfully registred. Now you can login" });
+        }
+    }
+    if(username){
+        return res.status(404).json({ message: "Unable to register user. password not provided!" });
+    }else{
+        return res.status(404).json({ message: "Unable to register user. username not provided!" });
+    }
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+    //Write your code here
+    const author = req.params.author;
+    let bkArr = [];
+    for (var key in books) {
+        const book = books[key];
+        if (book.author === author) {
+            bkArr.push(book);
+        }
+    }
+    res.send(bkArr);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    let bkArr = [];
+    for (var key in books) {
+        const book = books[key];
+        if (book.title === title) {
+            bkArr.push(book);
+        }
+    }
+    res.send(bkArr);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
